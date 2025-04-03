6ad694557966c500b9ea00cdb5c9fb904452e758
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-03 12:19:41 -0600

    Update alicia_viz.py
    Constructor logic is complete.

--- code/src/rbc_gem_utils/alicia_viz.py
+++ code/src/rbc_gem_utils/alicia_viz.py
@@ -3,22 +3,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.gridspec import GridSpec
+from rbc_gem_utils import read_cobra_model
 
 
 class FluxOptimizationViz:
-    def __init__(self, model, df_pcfva_alleles):
+    def __init__(self, model_filename, df_pcfva_alleles_filename):
         """
         Instantiate an object to create flux optimization visualizations.
 
         Parameters
         ----------
-        model : cobra.Model
-            An instance of the non-protein-constrained model from which
-            to obtain data about reactions.
+        model_filename : str
+            Path to the non-protein-constraied COBRA model from which to
+            obtain data about reactions.
 
-        df_pcfva_alleles : pd.DataFrame
-            DataFrame with the following columns: day, reactions, optimum,
-            G6PD_alleles, minimum, maximum, range, and sample_id
+        df_pcfva_alleles_filename : str
+            Path to a csv DataFrame with the following columns: day,
+            reactions, optimum, G6PD_alleles, minimum, maximum, range,
+            and sample_id.
         """
-        self.model = model
+        self.model = read_cobra_model(model_filename)
+        df_pcfva_alleles = pd.read_csv(df_pcfva_alleles_filename)
+        df_pcfva_alleles.drop("sample_id", axis=1, inplace=True)
+        df_pcfva_alleles.set_index(
+            ["G6PD_alleles", "day", "reactions", "optimum"], inplace=True
+        )
+        df_pcfva_alleles.sort_index(inplace=True)
         self.df_pcfva_alleles = df_pcfva_alleles
