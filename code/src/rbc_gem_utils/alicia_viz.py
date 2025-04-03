d07301d729afbde3227cfd21323f73c87b87293f
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-03 11:42:13 -0600

    Update alicia_viz.py
    A class to create flux optimization plots.

---
+++ code/src/rbc_gem_utils/alicia_viz.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.gridspec import GridSpec
+from rbc_gem_utils import read_cobra_model
+
+
+class FluxOptimizationViz:
+    def __init__(self):
+        pass
