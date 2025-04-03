a5d113c7162aa08bdfc1ab170131190075085cf2
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-04-03 11:51:57 -0600

    Update alicia_viz.py
    Create a constructor for the class.

--- code/src/rbc_gem_utils/alicia_viz.py
+++ code/src/rbc_gem_utils/alicia_viz.py
@@ -3,9 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.gridspec import GridSpec
-from rbc_gem_utils import read_cobra_model
 
 
 class FluxOptimizationViz:
-    def __init__(self):
-        pass
+    def __init__(self, model, df_pcfva_alleles):
+        """
+        Instantiate an object to create flux optimization visualizations.
+
+        Parameters
+        ----------
+        model : cobra.Model
+            An instance of the non-protein-constrained model from which
+            to obtain data about reactions.
+
+        df_pcfva_alleles : pd.DataFrame
+            DataFrame with the following columns: day, reactions, optimum,
+            G6PD_alleles, minimum, maximum, range, and sample_id
+        """
+        self.model = model
+        self.df_pcfva_alleles = df_pcfva_alleles
